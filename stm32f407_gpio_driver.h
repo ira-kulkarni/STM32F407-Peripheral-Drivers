d2eaf1f31ca65b83d6afa6dfa30da8a2d0e89560
ira-kulkarni <ira.kulkarni@gmail.com>
2026-08-19 14:31:15 -0700

    Fix header guard definition in gpio driver

--- stm32f407_gpio_driver.h
+++ stm32f407_gpio_driver.h
@@ -5,7 +5,7 @@
  *      Author: irakulkarni
  */
 
-#define INC_STM32F407XX_GPIO_DRIVER_H_
+#ifndef INC_STM32F407XX_GPIO_DRIVER_H_
 #define INC_STM32F407XX_GPIO_DRIVER_H_
 
 #include "stm32f407xx.h"
